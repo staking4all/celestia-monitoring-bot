6fe9a7cfab49dec9b8a1bdab3bf1c02895f96e61
staking4all <staking4all@staking4all.org>
2023-04-25 13:51:12 -0400

    fix DM handle middeware

--- services/telegram/middleware.go
+++ services/telegram/middleware.go
@@ -1,11 +1,13 @@
 package telegram
 
-import "gopkg.in/telebot.v3"
+import (
+	"gopkg.in/telebot.v3"
+)
 
 func OnlyDM() telebot.MiddlewareFunc {
 	return func(next telebot.HandlerFunc) telebot.HandlerFunc {
 		return func(c telebot.Context) error {
-			if msg := c.Message(); msg != nil && msg.Private() {
+			if msg := c.Message(); msg != nil && !msg.Private() {
 				return nil
 			}
 			return next(c)
