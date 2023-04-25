9b00faa6b65f6a62c8914de21e3b9f7e7f5e61de
staking4all <staking4all@staking4all.org>
2023-04-25 13:51:12 -0400

    handle remove cmd

--- services/telegram/validator.go
+++ services/telegram/validator.go
@@ -13,6 +13,7 @@ func (t *telegramNotificationService) addValidatorHandler(c telebot.Context) err
 		t.Send(c.Chat(), "Please try `/add ValidatorName celestiavalcons1XXXXXXX`")
 		return nil
 	}
+
 	zap.L().Info("add validator", zap.Int64("userID", c.Sender().ID), zap.String("userName", c.Sender().Username), zap.String("name", c.Args()[0]), zap.String("address", c.Args()[1]))
 	err := t.mm.Add(c.Sender().ID, models.NewValidator(c.Args()[0], c.Args()[1]))
 	if err != nil {
@@ -20,12 +21,26 @@ func (t *telegramNotificationService) addValidatorHandler(c telebot.Context) err
 		return err
 	}
 
-	t.Send(c.Chat(), fmt.Sprintf("validator added monitor list *%s*", c.Args()[1]))
+	t.Send(c.Chat(), fmt.Sprintf("validator added to monitor list *%s*", c.Args()[1]))
 
 	return nil
 }
 
 func (t *telegramNotificationService) removeValidatorHandler(c telebot.Context) error {
+	if len(c.Args()) != 1 {
+		// TODO: list validators regitered to user
+		t.Send(c.Chat(), "Please try `/remove celestiavalcons1XXXXXXX`")
+		return nil
+	}
+
+	zap.L().Info("remove validator", zap.Int64("userID", c.Sender().ID), zap.String("userName", c.Sender().Username), zap.String("address", c.Args()[0]))
+	err := t.mm.Remove(c.Sender().ID, c.Args()[0])
+	if err != nil {
+		t.Send(c.Chat(), err.Error())
+		return err
+	}
+
+	t.Send(c.Chat(), fmt.Sprintf("validator removed from monitor list *%s*", c.Args()[0]))
 
 	return nil
 }
